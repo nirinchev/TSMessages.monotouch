6863b7414c08394bf5fc78c8083d94d573f5fa49
Nikola Irinchev <irinchev@me.com>
2015-03-16 17:08:10 +0200

    Initial Commit

---
+++ XCode project/TSMessagesWrapper/TSMessagesWrapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSMessagesWrapper
+//
+//  Created by Nikola Irinchev on 3/16/15.
+//  Copyright (c) 2015 Nikola Irinchev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCode project/TSMessagesWrapper/TSMessagesWrapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TSMessagesWrapper
+//
+//  Created by Nikola Irinchev on 3/16/15.
+//  Copyright (c) 2015 Nikola Irinchev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
